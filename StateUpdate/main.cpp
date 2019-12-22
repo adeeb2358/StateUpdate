5fd17d5e6f63fd259f1e02d11e9563b0b1ed5a63
Adeeb Mohammed <adeeb2358@gmail.com>
2019-12-22 19:20:55 +0900

    Initial Commit

---
+++ StateUpdate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StateUpdate
+//
+//  Created by adeeb mohammed on 22/12/19.
+//  Copyright © 2019 adeeb mohammed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
